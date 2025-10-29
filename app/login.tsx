73f5eaa005dddbdfd81b8c43de5b6a35796fc1e9
antonkatyrenchuk <anton.katyrenchuk@gmail.com>
2025-10-29 14:22:33 +0100

    Add UI enhancements for HomeScreen, LoginScreen, and MapScreen
    - Introduced EventCard and LectureCard components on HomeScreen.
    - Enhanced LoginScreen with email/password fields and forgot password/help options.
    - Updated MapScreen with detailed event info and "Show route" button.
    - Added title to Home layout stack.
    - Improved ThemedButton styling.

--- app/login.tsx
+++ app/login.tsx
@@ -1,28 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import {Platform, StyleSheet, TextInput, View} from "react-native";
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import {ThemedButton, ThemedText, ThemedView} from "@/components/Themed";
+import {ThemedButton, ThemedText, ThemedView, useThemeColor} from "@/components/Themed";
 import {useUser} from "@/app/user-ctx";
+import {ms, ScaledSheet} from "react-native-size-matters";
+import {Input} from "postcss";
+import {Image} from "expo-image";
+import {IconSymbol} from "@/components/IconSymbol";
 
 export default function LoginScreen() {
   const {setStatus} = useUser();
+  const backgroundColor = useThemeColor({ }, 'background');
 
   return (
-    <ThemedView style={styles.container}>
+    <View style={[{backgroundColor}, styles.container]}>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-      <ThemedText style={styles.title}>Modal</ThemedText>
-      <ThemedView style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <ThemedButton textContent={"Close modal"} onPress={() => setStatus("ready")}/>
-      <EditScreenInfo path="app/modal.tsx" />
-    </ThemedView>
+      <Image
+        source={'bi_logo_dark'}
+        style={{
+          width: ms(48),
+          height: ms(48)
+        }}
+      />
+      <ThemedText type={"titleLarge"}>Log in</ThemedText>
+      <View style={styles.sectionContainer}>
+        <ThemedText type={"title"}>Email</ThemedText>
+        <TextInput style={styles.input} placeholder={"Your BI email"}>
+
+        </TextInput>
+      </View>
+      <View style={styles.sectionContainer}>
+        <ThemedText type={"title"}>Password</ThemedText>
+        <TextInput style={styles.input} placeholder={"Your BI email"} secureTextEntry={true}>
+
+        </TextInput>
+      </View>
+      <ThemedButton textContent={"Log in"} onPress={() => setStatus("ready")}/>
+      <View style={{
+        alignSelf: "flex-end",
+        borderBottomWidth: 3,
+        borderBottomColor: '#597694'
+      }}>
+        <ThemedText type={"defaultBold"}>Forgot your password?</ThemedText>
+      </View>
+      <View/>
+      <View/>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <View style={{
+          borderBottomWidth: 3,
+          borderBottomColor: '#597694'
+        }}>
+          <ThemedText type={"defaultBold"}>Help</ThemedText>
+        </View>
+        <IconSymbol name={"globe"} color={'#597694'} weight={"bold"} size={ms(24)}/>
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = ScaledSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    padding: '12@ms',
+    gap: '24@ms'
   },
   title: {
     fontSize: 20,
@@ -33,4 +78,14 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  sectionContainer: {
+    gap: '6@ms'
+  },
+  input: {
+    paddingVertical: '12@ms',
+    paddingHorizontal: '12@ms',
+    borderWidth: '0.5@ms',
+    borderRadius: '4@ms',
+    borderColor: '#597694'
+  }
 });
