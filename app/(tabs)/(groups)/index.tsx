df9b919ec0318128df9f5c140a4e6475f4fd8da9
alexandragrassi <oleksandra.hrassi@bi.no>
2025-10-29 14:16:20 +0100

    Render groups list in GroupsScreen using GroupCard with dummy data

--- app/(tabs)/(groups)/index.tsx
+++ app/(tabs)/(groups)/index.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/Themed';
+import { groupsList} from "@/dummy-data/groups";
+import GroupCard from "@/components/GroupCard";
 
 export default function GroupsScreen() {
   return (
     <ThemedView style={styles.container}>
-
+      {groupsList.map((group, index) => (
+        <GroupCard title={group.name} key={group.name}></GroupCard>
+      ))}
     </ThemedView>
   );
 }
